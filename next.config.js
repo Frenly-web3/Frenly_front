f6ae0b27508e9e105deb3750ea641f40f6be7a51
Aleksey <klimenkoaleksey02@gmail.com>
2022-08-01 21:13:59 +0300

    feat: edit README

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 })
 
 module.exports = withBundleAnalyzer({
