affb981eda7cd8bdd6c1a439bfb9893c11461cf5
Alexander <das3d@bk.ru>
2022-09-14 19:06:30 +0300

    feat: add pages

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,14 +1,67 @@
-import '../styles/global.css'
+import '../styles/global.scss'
 
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client'
 import { store } from '@store/store'
+import type { Config } from '@usedapp/core'
+import { ChainId, DAppProvider } from '@usedapp/core'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 
+const config: Config = {
+  readOnlyChainId: ChainId.BSCTestnet,
+  readOnlyUrls: {
+    // [ChainId.Rinkeby]: infuraProvider,
+    // [ChainId.BSC]: "https://bsc-dataseed.binance.org/",
+    // [ChainId.BSCTestnet]: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+    [ChainId.Mumbai]:
+      'https://polygon-mumbai.g.alchemy.com/v2/xuvttDBAAQvHjBMWurgJzXCKjsWyp8x_',
+  },
+  notifications: {
+    expirationPeriod: 1000,
+    checkInterval: 200_000,
+  },
+  autoConnect: true,
+}
+
+const httpLink = new HttpLink({ uri: 'https://api-mumbai.lens.dev/' })
+
+// example how you can pass in the x-access-token into requests using `ApolloLink`
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authorization token from local storage.
+  // if your using node etc you have to handle your auth different
+  const token = localStorage.getItem('auth_token')
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      'x-access-token': token ? `Bearer ${token}` : '',
+    },
+  })
+
+  // Call the next link in the middleware chain.
+  return forward(operation)
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <ApolloProvider client={client}>
+      <DAppProvider config={config}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </DAppProvider>
+    </ApolloProvider>
   )
 }
 
---
+++ src/pages/myEvents/index.page.tsx
@@ -0,0 +1,58 @@
+import Event from '@components/shared/event/event.component'
+import styles from '@pages/myEvents/myEvents.module.scss'
+import React from 'react'
+
+const eventsMock = [
+  {
+    from: '0x0e2f7D1a076100059824c14021919eFB509bA25b',
+    to: '0x0e2f7D1a076100059824c14021919eFB509bA25b',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: 'https://miro.medium.com/max/1400/1*cdn3L9ehKspSxiRJfRYSyw.png',
+  },
+  {
+    from: '0x0e2f7D1a076100059824c14021919eFB509bA25b',
+    to: '0x0e2f7D1a076100059824c14021919eFB509bA25b',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: 'https://miro.medium.com/max/1400/1*cdn3L9ehKspSxiRJfRYSyw.png',
+  },
+  {
+    from: '0x0e2f7D1a076100059824c14021919eFB509bA25b',
+    to: '0x0e2f7D1a076100059824c14021919eFB509bA25b',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: 'https://miro.medium.com/max/1400/1*cdn3L9ehKspSxiRJfRYSyw.png',
+  },
+]
+
+export default function MyEventsPage() {
+  // const addPost = () => {}
+
+  // const declinePost = () => {}
+
+  return (
+    <div className={styles.myEvents}>
+      <div className={styles.content}>
+        <div className={styles.address}>
+          {`${'0x0e2f7D1a076100059824c14021919eFB509bA25b'.slice(
+            0,
+            7
+          )}...${'0x0e2f7D1a076100059824c14021919eFB509bA25b'.slice(-7)}`}
+        </div>
+        <h3 className={styles.eventsTitle}>My Events</h3>
+        <div className={styles.events}>
+          {eventsMock.map((el, index) => {
+            return (
+              <Event
+                isAddCap
+                from={el.from}
+                to={el.to}
+                info={el.info}
+                image={el.image}
+                key={index}
+              />
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
