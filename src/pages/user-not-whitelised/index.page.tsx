655ceab79de9484e0106f25b9c3c3666d9d80431
fedyashevvic <fedyashevvic@gmail.com>
2022-12-10 11:25:15 +0800

    adds mvp whitelist block

---
+++ src/pages/user-not-whitelised/index.page.tsx
@@ -0,0 +1,26 @@
+import { Button, Meta } from '@shared/ui'
+import { useRouter } from 'next/router'
+
+export default function UserNotWhitelistedPage() {
+  const router = useRouter()
+
+  return (
+    <>
+      <Meta title="Frenly" description="Not whitelisted" />
+      <div className="container flex flex-col items-center h-screen">
+        <div className="flex flex-1 flex-col items-center justify-center">
+          <img src="/assets/images/eyes.gif" alt="eyes" className="w-20 h-20" />
+          <h1 className="text-4xl font-bold mt-16">Frenly</h1>
+          <h2 className="text-base text-center mt-3 text-gray">
+            Woops, your address in not whitelisted for alpha test. Gain access to the closed alpha filling the form below.
+          </h2>
+        </div>
+        <div className="w-full py-4 mb-16">
+          <a href='https://discord.gg/tBhYAaGz' className="w-full"><Button>Open Form</Button></a>
+          <div className="mt-2"></div>
+          <Button onClick={() => router.push(`/auth`)}>Back to Auth</Button>
+        </div>
+      </div>
+    </>
+  )
+}
