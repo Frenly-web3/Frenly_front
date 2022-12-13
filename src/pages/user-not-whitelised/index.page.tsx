6898ed45543728ab7efead80dbebf70b90fc1c99
fedyashevvic <fedyashevvic@gmail.com>
2022-12-13 17:24:50 +0800

    prod redeploy

--- src/pages/user-not-whitelised/index.page.tsx
+++ src/pages/user-not-whitelised/index.page.tsx
@@ -12,11 +12,15 @@ export default function UserNotWhitelistedPage() {
           <img src="/assets/images/eyes.gif" alt="eyes" className="w-20 h-20" />
           <h1 className="text-4xl font-bold mt-16">Frenly</h1>
           <h2 className="text-base text-center mt-3 text-gray">
-            Woops, your address in not whitelisted for alpha test. Gain access to the closed alpha filling the form below.
+            Woops, your address in not whitelisted for alpha test. Gain access to the
+            closed alpha filling the form below.
           </h2>
         </div>
+
         <div className="w-full py-4 mb-16">
-          <a href='https://1ltwty44asn.typeform.com/to/DQ8hHy11' className="w-full"><Button>Open Form</Button></a>
+          <a href="https://1ltwty44asn.typeform.com/to/DQ8hHy11" className="w-full">
+            <Button>Open Form</Button>
+          </a>
           <div className="mt-2"></div>
           <Button onClick={() => router.push(`/auth`)}>Back to Auth</Button>
         </div>
