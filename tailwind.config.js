b2d0f98a66192cc4a08688c5f1f5949bad2d90f8
Denis Shutock <yoshutock@gmail.com>
2022-12-22 05:16:16 +0600

    style: new theme for tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,15 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      'sans': ['SF Pro Text']
+      // Old
+      'sans': ['SF Pro Text'],
+
+      // New
+      'compact' : ['SF Compact Display'],
+      'mono' : ['SF Mono'],
+      'display' : ['SF Pro Display'],
+      'rounded' : ['SF Pro Rounded'],
+      'text' : ['SF Pro Text'],
     },
     container: {
       center: true,
@@ -25,7 +33,7 @@ module.exports = {
     colors: {
       // Old
       'main': '#248BF2',
-      'white': '#ffffff',
+      // 'white': '#ffffff',
       'gray': '#71747A',
       'light-gray':'rgba(201, 204, 209, 0.24)',
       'gray-darker': '#81889F',
@@ -36,6 +44,40 @@ module.exports = {
       'is-liked': 'rgba(252, 165, 165, 0.8 )',
 
       // New
+      'white': 'hsla(0, 0%, 100%, 100%)',
+      'black': 'hsla(0, 0%, 0%, 100%)',
+
+      'background': 'hsla(0, 0%, 100%, 100%)',
+
+      'heading': 'hsla(0, 0%, 0%, 100%)',
+      'text': 'hsla(0, 0%, 0%, 80%)',
+      'hidden': 'hsla(0, 0%, 0%, 40%)',
+
+      'overlay-1': 'hsla(0, 0%, 100%, 5%)',
+      'overlay-2': 'hsla(0, 0%, 100%, 10%)',
+      'overlay-3': 'hsla(0, 0%, 100%, 20%)',
+      'overlay-1-solid': 'hsla(0, 0%, 95%, 100%)',
+      'overlay-2-solid': 'hsla(0, 0%, 90%, 100%)',
+      'overlay-3-solid': 'hsla(0, 0%, 80%, 100%)',
+
+      'border': 'hsla(0, 0%, 0%, 20%)',
+      'border-solid': 'hsla(0, 0%, 20%, 100%)',
+
+      'success-light': 'hsla(150, 90%, 40%, 100%)',
+      'success-dark': 'hsla(150, 90%, 40%, 100%)',
+      'success-tint': 'hsla(150, 90%, 40%, 100%)',
+      
+      'information-light': 'hsla(210, 90%, 55%, 100%)',
+      'information-dark': 'hsla(210, 90%, 35%, 100%)',
+      'information-tint': 'hsla(210, 90%, 55%, 40%)',
+      
+      'warning-light': 'hsla(40, 100%, 50%, 100%)',
+      'warning-dark': 'hsla(40, 100%, 30%, 100%)',
+      'warning-tint': 'hsla(40, 100%, 50%, 400%)',
+      
+      'error-light': 'hsla(350, 100%, 50%, 100%)',
+      'error-dark': 'hsla(350, 100%, 30%, 100%)',
+      'error-tint': 'hsla(350, 100%, 50%, 40%)',
     }
   },
   plugins: [require('tw-elements/dist/plugin')],
