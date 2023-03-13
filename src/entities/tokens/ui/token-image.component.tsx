dd7bb935ebc4da6cc725265240efc4d3cf1e7cca
das3d <das3d@bk.ru>
2023-03-13 22:16:25 +0300

    fix: profile crash on safari

--- src/entities/tokens/ui/token-image.component.tsx
+++ src/entities/tokens/ui/token-image.component.tsx
@@ -6,40 +6,25 @@ export interface ITokenImageProps {
 
 export function TokenImage(props: ITokenImageProps) {
   const { image } = props;
-  const [errorImage, setErrorImage] = React.useState(false);
-  const [load, setLoad] = React.useState(false);
+  // const [errorImage, setErrorImage] = React.useState(false);
+  // const [load, setLoad] = React.useState(false);
 
   return (
     <>
-      {!errorImage ? (
-        <div
-          className={`w-full aspect-square rounded-2xl overflow-hidden border-2 border-black/10 ${
-            load && "bg-black/20 animate-pulse relative"
-          }`}
-        >
-          <img
-            width={139}
-            height={139}
-            src={image}
-            onError={() => setErrorImage(true)}
-            alt=""
-            onLoadStart={() => setLoad(true)}
-            loading="lazy"
-            className="w-full aspect-square"
-          />
-        </div>
-      ) : (
-        <div
-          className={`w-full aspect-square rounded-2xl overflow-hidden border-2 border-black/10 ${
-            load && "bg-black/20 animate-pulse"
-          }`}
-        >
-          <video src={image} className="w-full">
-            <source src={image} className="w-full" />
-            <img src={image} className="w-full" />
-          </video>
-        </div>
-      )}
+      <div
+        className={`w-full aspect-square rounded-2xl overflow-hidden border-2 border-black/10 `}
+      >
+        <img
+          width={139}
+          height={139}
+          src={image}
+          // onError={() => setErrorImage(true)}
+          alt=""
+          // onLoadStart={() => setLoad(true)}
+          // loading="lazy"
+          className="w-full aspect-square"
+        />
+      </div>
     </>
   );
 }
