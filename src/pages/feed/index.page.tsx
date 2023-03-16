756a07d357dd884dd79029a13ff676b83db03c95
das3d <das3d@bk.ru>
2023-03-16 17:32:05 +0300

    fix: comment push button

--- src/pages/feed/index.page.tsx
+++ src/pages/feed/index.page.tsx
@@ -1,4 +1,4 @@
-import { useConnectPush } from "@features/connect-push-notifications";
+// import { useConnectPush } from "@features/connect-push-notifications";
 import { useChangeAddress } from "@widgets/change-address";
 import { Layout } from "@widgets/layout";
 
@@ -8,19 +8,19 @@ import { RightSection } from "./right-section.component";
 export default function FeedPage() {
   useChangeAddress();
 
-  const { connectPush, subscribed } = useConnectPush();
-  console.log(subscribed);
+  // const { connectPush, subscribed } = useConnectPush();
+  // console.log(subscribed);
 
   return (
     <Layout title="feed" rightSidebar={<RightSection />}>
-      <button
+      {/* <button
         onClick={async () => {
           // await requestPermission();
           await connectPush();
         }}
       >
         Send push
-      </button>
+      </button> */}
       <section className="lg:max-w-[37rem] min-w-[18.5rem] md:px-4 relative">
         <InfinitePosts />
       </section>
